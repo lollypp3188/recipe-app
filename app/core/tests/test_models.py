4970137c590fe29c9a7c0ea62289752cadd0f69a
lollypp3188 <annayakymovaaa@gmail.com>
2024-08-14 20:12:36 +0300

    test: add test tags api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,14 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(
+        email='user@gmail.com',
+        password='testpass123'
+):
+    """Create and return new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -62,3 +70,9 @@ class ModelTests(TestCase):
             description='Simple description.',
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+        self.assertEqual(str(tag), tag.name)
